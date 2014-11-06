3be960e4d07f6a7c0ee458a38c2b62a87c1f97ac
Songmu <y.songmu@gmail.com>
2014-11-06 20:24:19 +0900

    initial implement

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::KV2JSON - It's new $module
+
+# SYNOPSIS
+
+    use App::KV2JSON;
+
+# DESCRIPTION
+
+App::KV2JSON is ...
+
+# LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Songmu <y.songmu@gmail.com>
---
+++ lib/App/KV2JSON.pm
@@ -0,0 +1,39 @@
+package App::KV2JSON;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::KV2JSON - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::KV2JSON;
+
+=head1 DESCRIPTION
+
+App::KV2JSON is ...
+
+=head1 LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Songmu E<lt>y.songmu@gmail.comE<gt>
+
+=cut
+

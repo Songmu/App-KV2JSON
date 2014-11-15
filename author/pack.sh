23680de9c3dba75b300a2500fb814736fbaa12c2
Songmu <y.songmu@gmail.com>
2014-11-15 15:55:35 +0900

    use Perl::Strip for maintenance

--- author/pack.sh
+++ author/pack.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-fatpack trace -Ilib script/kv2json!
+set -e
+set -x
+src="script/kv2json"
+dst="kv2json"
+
+fatpack trace -Ilib $src
 fatpack packlists-for `cat fatpacker.trace` > fatpacker.packlists
-(fatpack file; cat script/kv2json ) > kv2json
+
+if type perlstrip >/dev/null 2>&1; then
+    find fatlib -type f -name '*.pm' | xargs -n1 perlstrip -s
+fi
+
+(echo "#!/usr/bin/env perl"; fatpack file; cat $src ) > $dst
+chmod +x $dst
